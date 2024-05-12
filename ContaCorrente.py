14ebee05a57c420962407f52cd1a3c54f5c578b1
Jonnathas <kjonnathas@gmail.com>
2024-05-11 21:52:20 -0300

    new modifications to the main file

--- ContaCorrente.py
+++ ContaCorrente.py
@@ -595,29 +595,3 @@ class ContaCorrente:
                                         else:
                                             print('A resposta informada é inválida. Por favor, digite novamente.')
                     print('O valor informado é inválido. Por favor, digite novamente. Caso queira cancelar a operação digite "Cancelar".')
-
-
-if __name__ == '__main__':
-
-    conta = ContaCorrente()
-
-    # cadastrar_cliente(
-    #     nome_cliente=conta.nome,
-    #     genero=conta.genero,
-    #     cpf=conta.cpf,
-    #     rg=conta.rg,
-    #     data_nascimento=conta.data_nascimento,
-    #     email=conta.email,
-    #     celular=conta.celular,
-    #     renda=conta.renda,
-    #     agencia=conta.agencia,
-    #     conta=conta.conta,
-    #     saldo=conta.saldo,
-    #     rua=conta.endereco['rua'],
-    #     numero = conta.endereco['numero'],
-    #     bairro=conta.endereco['bairro'],
-    #     cidade=conta.endereco['cidade'],
-    #     uf=conta.endereco['uf'],
-    #     cep=conta.endereco['cep'],
-    #     data_atualizacao=conta._data_hora()
-    # )
